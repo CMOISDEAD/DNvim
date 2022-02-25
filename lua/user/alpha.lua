fb9995f88b6328e0c03770d197fb7e2c0825e16a
CMOISDEAD <camilodavina@gmail.com>
2022-02-24 21:38:24 -0500

    Configuring

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "dropdeadcmo.com"
 end
 
 dashboard.section.footer.val = footer()
