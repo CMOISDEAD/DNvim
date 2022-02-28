15fd653e2f3dd7e1cd991204be67e4d2e1064b15
camilo <camilodavina@gmail.com>
2022-02-27 21:13:05 -0500

    Indent Block

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -3,14 +3,13 @@ if not status_ok then
 	return
 end
 
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+vim.g.indent_blankline_buftype_exclude = { "terminal" }
 vim.g.indent_blankline_filetype_exclude = {
 	"help",
 	"startify",
 	"dashboard",
 	"packer",
 	"neogitstatus",
-	"NvimTree",
 	"Trouble",
 }
 vim.g.indentLine_enabled = 1
